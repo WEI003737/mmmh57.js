2e7362749f27abe6d77a08e90ceb891f8622d2e3
WEI003737 <62680270+WEI003737@users.noreply.github.com>
2020-03-30 16:16:33 +0800

    get

--- test/a20200330-03-basic.php
+++ test/a20200330-03-basic.php
@@ -1,14 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>a20200330-03-basic</title>
-</head>
-<body>
+
 <?php
+    //isset用來判斷有沒有設定過此變數
     $a = isset($_GET['a']) ? intval($_GET['a']) : 0;
     $b = isset($_GET['b']) ? intval($_GET['b']) : 0;
 
@@ -22,5 +14,3 @@
     echo '</tr></table>';
 
 ?>
-</body>
-</html>
