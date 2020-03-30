fa599dc57d10629663790bc17f6caaf7d3d13b42
WEI003737 <62680270+WEI003737@users.noreply.github.com>
2020-03-30 14:05:31 +0800

    test

--- test/a20200330-02-basic.php
+++ test/a20200330-02-basic.php
@@ -10,19 +10,10 @@
 <body>
 <?php
 
-//系統功能
-echo PHP_VERSION.'<br>';
-echo __DIR__.'<br>'; //本php檔案所在資料夾
-echo __FILE__.'<br>'; //檔案完整路徑
-echo __LINE__.'<br>'; //標記行數:除錯
+$my = 123;
 
-//常數
-define('MY_CONST', 3333); //define不區分大小寫
-echo MY_CONST.'<br>';
+echo $my;
 
-//布林值不區分大小寫
-echo FALSE.'<br>'; //空字串
-echo true.'<br>';
 ?>
 </body>
 </html>
