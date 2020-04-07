c7ba2358d0a5adda00757a30b20acf406019109c
WEI003737 <virginial2123@gmail.com>
2020-04-07 14:32:15 +0800

    SQL

--- test/a20200406-10-mySQL.sql
+++ test/a20200406-10-mySQL.sql
@@ -7,4 +7,16 @@ CREATE TABLE `proj57`.`product_top` (
 `creat_date` DATETIME NOT NULL ,
 PRIMARY KEY (`sid`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
 
-INSERT INTO `product_top` (`sid`, `item_name`, `item_num`, `color`, `color_num`, `creat_date`) VALUES (NULL, 'sweater', '00', 'r', '00', '2020-04-06 16:48:10');
+INSERT INTO
+`product_top`
+(`sid`, `item_name`, `item_num`, `color`, `color_num`, `creat_date`)
+VALUES
+(NULL, 'sweater', '00', 'r', '00', '2020-04-06 16:48:10'),
+(NULL, 'sweater', '00', 'r', '01', '2020-04-06 16:48:10'),
+(NULL, 'sweater', '00', 'r', '02', '2020-04-06 16:48:10'),
+(NULL, 'sweater', '00', 'r', '03', '2020-04-06 16:48:10'),
+(NULL, 'sweater', '00', 'r', '04', '2020-04-06 16:48:10'),
+(NULL, 'sweater', '00', 'r', '05', '2020-04-06 16:48:10');
+
+DELETE FROM `product_top` WHERE `product_top`.`sid` = 4;
+UPDATE `product_top` SET `color`= 'bk' WHERE `sid` = 4;
