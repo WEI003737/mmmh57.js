8e351b724b97165da5c52bfaaff415e0b3fe7652
WEI <virginial2123@gmail.com>
2020-04-14 00:32:04 +0800

    edit

--- test/__connect_db.php
+++ test/__connect_db.php
@@ -8,7 +8,9 @@ $db_pass = '';
 $dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8', $db_host, $db_name);
 
 $pdo_options = [
+    //設定資料庫錯誤時的例外
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    //使用fetch style: 關聯式陣列
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
 ];
