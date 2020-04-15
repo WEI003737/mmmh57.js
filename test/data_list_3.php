809f19ba2a663b3128cc928744719e5331ad82f6
WEI <virginial2123@gmail.com>
2020-04-15 21:10:12 +0800

    data list 3

--- test/data_list_3.php
+++ test/data_list_3.php
@@ -9,50 +9,42 @@ $page_name = 'data-list3';
     <div class="container">
         <div class="row">
             <div class="col">
+                <form class="form-inline my-2 my-lg-0" onsubmit="return false;">
+                    <input id="searchInput" onkeyup="goPage(1)"
+                           class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <!--
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="button" onclick="goPage(1)">Search</button>
+                    -->
+                </form>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
                 <nav aria-label="Page navigation example">
                     <ul class="pagination">
-                        <!-- <li class="page-item">
-                            <a class="page-link" href="?page=">
-                                <i class="far fa-arrow-alt-circle-left"></i>
-                            </a>
-                        </li>
-
-                        <li class="page-item">
-                                <a class="page-link" href="?page=</a>
-                        </li>
-
-                        <li class="page-item">
-                            <a class="page-link" href="?page=">
-                                <i class="far fa-arrow-alt-circle-right"></i>
-                            </a>
-                        </li> -->
                     </ul>
                 </nav>
             </div>
         </div>
 
     <div class="row">
-        <table class="table table-striped table-bordered">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">item_name</th>
-                <th scope="col">item_num</th>
-                <th scope="col">color</th>
-                <th scope="col">color_num</th>
-            </tr>
-            </thead>
-            <tbody class="data-tbody">
-<!--                <tr>-->
-<!--                    <td></td>-->
-<!--                    <td></td>-->
-<!--                    <td></td>-->
-<!--                    <td></td>-->
-<!--                    <td></td>-->
-<!--                </tr>-->
-            </tbody>
-        </table>
-    </div>
+        <div class="col">
+            <table class="table table-striped table-bordered">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">item_name</th>
+                    <th scope="col">item_num</th>
+                    <th scope="col">color</th>
+                    <th scope="col">color_num</th>
+                </tr>
+                </thead>
+                    <tbody class="data-tbody">
+                </tbody>
+            </table>
+            </div>
+        </div>
     </div>
 <?php include __DIR__ . '/part/scripts.php' ?>
 <script>
@@ -67,7 +59,9 @@ $page_name = 'data-list3';
         tbody = $(".data-tbody");
 
     const escapeTag = str =>{
-        return str.split('&').join('&amp;').split('<').join('&lt;').split('>').join('&gt;')
+        return str.split('&').join('&amp;')
+            .split('<').join('&lt;')
+            .split('>').join('&gt;');
     };
 
     const itemTpl = o =>{
@@ -86,13 +80,13 @@ $page_name = 'data-list3';
         //{active:true, page:2}
       return`
         <li class="page-item ${obj.active ? 'active' : ''}">
-            <a class="page-link" href="#${obj.page}">${obj.page}</a>
+            <a class="page-link" href="javascript:goPage(${obj.page})">${obj.page}</a>
         </li>
       `;
     };
 
-    function getDataByPage(page=1) {
-        $.get('data_list_2_api.php', {page:page}, function(data){
+    function getDataByPage(page=1, keyword='') {
+        $.get('data_list_3_api.php', {page:page, keyword:keyword}, function(data){
             console.log(data)
             //頁碼 ---
             let pStr = '';
@@ -109,7 +103,7 @@ $page_name = 'data-list3';
             for(let i=0; i<data.rows.length; i++){
                 let item = data.rows[i];
                 // console.log(item)
-                tStr += itemTpl(item)
+                tStr += itemTpl(item);
             }
             tbody.html(tStr);
         }, 'json')
@@ -117,18 +111,42 @@ $page_name = 'data-list3';
     };
 
     function whenHashChange(){
-        let hashStr = location.hash.slice(1);
-        let page = parseInt(hashStr);
+        let hashStr = location.hash.slice(1); //去掉 #
+        console.log('hashStr:', hashStr);
+        console.log('hashStr2:', decodeURIComponent(hashStr));
+        hashStr = decodeURIComponent(hashStr); // url decode
+        let obj = {};
+
+        try{
+            obj = JSON.parse(hashStr); //把 json 轉換成物件
+        } catch(ex){
+            console.log(ex);
+        }
+        console.log('obj:', obj);
+        let page = obj.page;
+        const keyword = obj.keyword;
+
+        // 由 hash 設定到搜尋欄
+        if(keyword !== $('#searchInput').val()){
+            $('#searchInput').val(keyword);
+        }
 
         if(page){
-            getDataByPage(page);
-        }else {
-            getDataByPage(1);
+            getDataByPage(page, keyword);
+        } else {
+            getDataByPage(1, keyword);
         }
     };
 
     window.addEventListener("hashchange", whenHashChange);
     whenHashChange();
+
+    function goPage(page=1){
+        location.href = '#' + JSON.stringify({
+            page: page,
+            keyword: $('#searchInput').val()
+        });
+    }
 
 </script>
 
--- test/data_list_3_api.php
+++ test/data_list_3_api.php
@@ -3,10 +3,17 @@ require __DIR__ . '/__connect_db.php';
 
 $perPage = 5;
 $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
-$keyword = isset($_GET['keyword'])
+$keyword = isset($_GET['keyword']) ? $_GET['keyword'] : '';
+
+$where = ' WHERE 1 ';
+
+if(! empty($keyword)) {
+    $where .= " AND `item_name` LIKE ". $pdo->quote("%${keyword}%"); // 跳脫
+}
 
 //取得總筆數
-$totalRows = $pdo->query("SELECT COUNT(1) FROM `product_top`")->fetch(PDO::FETCH_NUM)[0];
+$totalRows = $pdo->query("SELECT COUNT(1) FROM `product_top` $where")
+    ->fetch(PDO::FETCH_NUM)[0];
 
 //算總頁數
 $totalPages = ceil($totalRows / $perPage);
@@ -14,17 +21,20 @@ $totalPages = ceil($totalRows / $perPage);
 ($page < 1) ? ($page = 1) : false;
 ($page > $totalPages) ? ($page = $totalPages) : false;
 
-
-$sql = sprintf("SELECT * FROM `product_top` LIMIT %s,%s", ($page - 1) * $perPage, $perPage);
-
-$stmt = $pdo->query($sql);
+$rows = [];
+// 避免沒有資料的錯誤
+if($totalPages>0){
+    $sql = sprintf("SELECT * FROM `product_top` %s ORDER BY `sid` DESC LIMIT %s, %s",
+        $where, ($page - 1) * $perPage, $perPage);
+    $rows = $pdo->query($sql)->fetchAll();
+}
 
 $output = [
     'page' => $page,
     'perPage' => $perPage,
     'totalRows' => $totalRows,
     'totalPages' => $totalPages,
-    'rows' => $stmt->fetchAll(),
+    'rows' => $rows,
 ];
 
 //告訴前端資料要用json格式
