56cdba79529ce7de8caf5c7fd934506ef62e041f
WEI003737 <virginial2123@gmail.com>
2020-05-07 18:02:44 +0800

    0507

---
+++ test/data_list_3_test.php
@@ -0,0 +1,184 @@
+<?php
+require __DIR__ . '/__connect_db.php';
+$page_name = 'data-list3';
+?>
+
+<?php include __DIR__ . '/part/header.php' ?>
+<?php include __DIR__ . '/part/nav.php' ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col categories">
+<!--                <a href="" datd-cate="1" onclick="goPage(1)">cate1</a>-->
+<!--                <a href="" datd-cate="2" onclick="goPage(1)">cate2</a>-->
+<!--                <a href="" datd-cate="3" onclick="goPage(1)">cate3</a>-->
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <form class="form-inline my-2 my-lg-0" onsubmit="return false;">
+                    <input id="searchInput" onkeyup="goPage(1)"
+                           class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <!--
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="button" onclick="goPage(1)">Search</button>
+                    -->
+                </form>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <table class="table table-striped table-bordered">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">item_name</th>
+                        <th scope="col">item_num</th>
+                        <th scope="col">color</th>
+                        <th scope="col">color_num</th>
+                    </tr>
+                    </thead>
+                    <tbody class="data-tbody">
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+<?php include __DIR__ . '/part/scripts.php' ?>
+    <script>
+        /*
+        運作的流程 steps
+        1.取得資料 (包成function)
+        2.生頁碼列的按鈕
+        3.生資料表格
+        */
+
+        const pagination = $(".pagination"),
+            tbody = $(".data-tbody"),
+            categories = $(".categories");
+
+        const escapeTag = str =>{
+            return str.split('&').join('&amp;')
+                .split('<').join('&lt;')
+                .split('>').join('&gt;');
+        };
+        const cateTpl = cate =>{
+            return`
+            <a href="" class="cate" datd-cate="${cate}" onclick="goPage(1)">cate${cate}</a>
+            `
+        };
+
+        const itemTpl = o =>{
+            // console.log('o:', o)
+            return`
+            <tr>
+                <td>${escapeTag(o.sid)}</td>
+                <td>${escapeTag(o.item_name)}</td>
+                <td>${escapeTag(o.item_num)}</td>
+                <td>${escapeTag(o.color)}</td>
+                <td>${escapeTag(o.color_num)}</td>
+            </tr>`;
+        };
+
+        const paginationTpl = (obj) =>{
+            //{active:true, page:2}
+            return`
+        <li class="page-item ${obj.active ? 'active' : ''}">
+            <a class="page-link" href="javascript:goPage(${obj.page})">${obj.page}</a>
+        </li>
+      `;
+        };
+
+        function getDataByPage(page=1, keyword='', cate='') {
+            $.get('data_list_3_api.php', {page:page, keyword:keyword, cate:cate }, function(data){
+                console.log(data)
+                let cStr = '';
+                for(let i=1; i<=3; i++){
+                    cStr += cateTpl(i)
+                }
+                categories.html(cStr);
+
+                //頁碼 ---
+                let pStr = '';
+                for(let i=1; i<=data.totalPages; i++){
+                    pStr += paginationTpl({
+                        active: page===i,
+                        page: i
+                    })
+                }
+                pagination.html(pStr);
+
+                //資料表格 ---
+                let tStr = '';
+                for(let i=0; i<data.rows.length; i++){
+                    let item = data.rows[i];
+                    // console.log(item)
+                    tStr += itemTpl(item);
+                }
+                tbody.html(tStr);
+            }, 'json')
+
+        };
+
+        function whenHashChange(){
+            let hashStr = location.hash.slice(1); //去掉 #
+            console.log('hashStr:', hashStr);
+            console.log('hashStr2:', decodeURIComponent(hashStr));
+            hashStr = decodeURIComponent(hashStr); // url decode
+            let obj = {};
+
+            try{
+                obj = JSON.parse(hashStr); //把 json 轉換成物件
+            } catch(ex){
+                console.log(ex);
+            }
+            // console.log('obj:', obj);
+            let page = obj.page;
+            const keyword = obj.keyword;
+            const cate = obj.cate;
+
+            // 由 hash 設定到搜尋欄
+            if(keyword !== $('#searchInput').val()){
+                $('#searchInput').val(keyword);
+            }
+
+            if(page){
+                getDataByPage(page, keyword, cate);
+            } else {
+                getDataByPage(1, keyword, cate);
+            }
+        };
+
+        window.addEventListener("hashchange", whenHashChange);
+        whenHashChange();
+
+        $(document).on("click", "cate", function(){
+            cateSid = $(event.target).attr("data-cate")
+            console.log("click")
+        });
+
+        function goPage(page=1){
+
+            location.href = '#' + JSON.stringify({
+                page: page,
+                keyword: $('#searchInput').val(),
+                cate: "1"
+                })
+
+        }
+
+    </script>
+
+<?php include __DIR__ . '/part/footer.php' ?>
